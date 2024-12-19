15afa98439d500ecfb14ff9ef8d184d501c85233
Kuzzuri <umuttengiz@gmail.com>
2024-12-19 18:18:34 +0300

    animation

---
+++ app.js
@@ -0,0 +1,56 @@
+const body = document.getElementById("body");
+const button = document.getElementById("button")
+let title = document.getElementById("title");
+let subTitle = document.getElementById("subTitle");
+const themeSetter = () => {
+    if(!body.classList.contains("active")){
+        button.style.opacity = 1;
+        title.style.opacity = 1;
+        subTitle.style.opacity = 1;
+        body.classList.add("active");
+
+    }
+    else{
+
+    }
+}
+const themeSetterOne= () => {
+    if(!body.classList.contains("unactive")){
+        body.classList.add("unactive");
+        title.style.transform = "translateY(1rem)";
+    }
+    else{
+
+    }
+}
+const themeSetterTwo= () => {
+    if(!body.classList.contains("unactive1")){
+        body.classList.add("unactive1");
+        subTitle.style.transform = "translateY(1rem)";
+    }
+    else{
+
+    }
+}
+const themeSetterThree= () => {
+    if(!body.classList.contains("unactive2")){
+        body.classList.add("unactive2");
+        button.style.transform = "translateY(1rem)";
+    }
+    else{
+
+    }
+}
+
+setInterval(() =>{
+    themeSetter()
+}, 500)
+setInterval(() =>{
+    themeSetterOne()
+}, 500)
+setInterval(() =>{
+    themeSetterTwo()
+}, 550)
+setInterval(() =>{
+    themeSetterThree()
+}, 600)
